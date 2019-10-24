5c93f93de3e5991430a4d1be66ad5b0aefda36be
Sue Raisty <sraisty@oneconcern.com>
2019-10-24 16:59:13 -0700

    Still works in the browser, but all test fail with the same   error: " TypeError: Cannot read property 'use' of undefined
        >  9 | i18n.use(initReactI18next).init(

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,4 +1,4 @@
-import i18next from 'i18next';
+import i18n, { TFunction } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 // import i18nextDefault, { TFunction } from 'i18next';
 import { appStrings } from '../../assets/i18n/appStrings.json';
@@ -6,16 +6,16 @@ import { appStrings } from '../../assets/i18n/appStrings.json';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 // const i18next: typeof i18nextDefault = require('i18next');
 
-i18next.use(initReactI18next).init(
+i18n.use(initReactI18next).init(
   {
     resources: appStrings,
     lng: 'en',
     fallbackLng: 'en',
     ns: ['translation', 'help', 'settings'],
   },
-  (err: Error, t: i18next.TFunction) => {
+  (err: Error, t: TFunction) => {
     console.log(`i18N initialized. Language: ${t('langCode')}`);
   },
 );
 
-export default i18next;
+export default i18n;
