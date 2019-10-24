38e5dd1370f1716099522f32e7bc1080566f0df1
Sue Raisty <sraisty@oneconcern.com>
2019-10-24 16:45:30 -0700

    Tests  pass, but web app shows type error: "Uncaught TypeError: i18next.use is not a function at Object../src/app/i18n.ts (i18n.ts:9) at __webpack_require__ (bootstrap:726)"

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,10 +1,10 @@
-import * as i18next from 'i18next';
+// import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-// import i18nextDefault from 'i18next';
+import i18nextDefault, { TFunction } from 'i18next';
 import { appStrings } from '../../assets/i18n/appStrings.json';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const i18next: typeof i18nextDefault = require('i18next');
+const i18next: typeof i18nextDefault = require('i18next');
 
 i18next.use(initReactI18next).init(
   {
@@ -13,7 +13,7 @@ i18next.use(initReactI18next).init(
     fallbackLng: 'en',
     ns: ['translation', 'help', 'settings'],
   },
-  (err, t) => {
+  (err: Error, t: TFunction) => {
     console.log(`i18N initialized. Language: ${t('langCode')}`);
   },
 );
