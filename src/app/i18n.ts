bff70b2f8ac3c2d5a3333d8892e73fbf5454e1c4
Sue Raisty <sraisty@oneconcern.com>
2019-10-24 21:04:31 -0700

    Handle i18next init promise with console.log/console.error messages.

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -8,13 +8,18 @@ import { appStrings } from '../../assets/i18n/appStrings.json';
 
 i18n.use(initReactI18next).init(
   {
+    debug: true,
     resources: appStrings,
     lng: 'en',
     fallbackLng: 'en',
     ns: ['translation', 'help', 'settings'],
   },
-  (err: Error, t: TFunction) => {
-    console.log(`i18N initialized. Language: ${t('langCode')}`);
+  (err: Error, t: i18n.TFunction) => {
+    if (err) {
+      console.error(`i18Next initialization error: ${err.message}`);
+    } else {
+      console.log(`i18N initialized. Language: ${t('langCode')}`);
+    }
   },
 );
 
