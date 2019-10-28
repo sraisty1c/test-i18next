916fb1984ac430c8bbdb528c9cade20e9a143fd6
Sue Raisty <sraisty@oneconcern.com>
2019-10-28 10:51:03 -0700

    Edit i18n to make clear the options I've tried for importing i18next , all of which have a problem with either the tests or the webapp.

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,9 +1,14 @@
-import * as i18n from 'i18next';
-
-// import * as i18nextDefault from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { appStrings } from '../../assets/i18n/appStrings.json';
 
+// 1. With following line, the webapp works but tests fail with type error
+import i18n from 'i18next'
+
+// 2. With the following line, tests pass but webapp fails in browser with type error
+// import * as i18n from 'i18next';
+
+// 3. With the next 3 lines, tests pass but webapp fails in browser
+// import * as i18nextDefault from 'i18next';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 // const i18n: typeof i18nextDefault = require('i18next');
 
