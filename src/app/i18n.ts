c5c1b1a5838ec35fb4b703e26136ffdc08801d6c
Sue Raisty <sraisty@oneconcern.com>
2019-10-27 23:36:13 -0700

    Now the web app works in the browser, but all the jest tests throw a type error:  'TypeError: Cannot read property 'use' of undefined' on line 9 of i18n.ts     >  9 | i18n.use(initReactI18next).init(

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,4 +1,4 @@
-import * as i18n from 'i18next';
+import i18n from 'i18next';
 // import * as i18nextDefault from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { appStrings } from '../../assets/i18n/appStrings.json';
